d0f0bcbc9afd9ce53aa0f33360d03ca90fa0fa02
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2020-11-13 08:27:54 +0000

    Clean up the unused import and fields.
    With the switch to the configurer we don’t need all those old imports and fields.

--- src/main/java/uk/ac/ox/ctl/lti13/demo/WebSecurityConfig.java
+++ src/main/java/uk/ac/ox/ctl/lti13/demo/WebSecurityConfig.java
@@ -1,34 +1,14 @@
 package uk.ac.ox.ctl.lti13.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import uk.ac.ox.ctl.lti13.Lti13Configurer;
-import uk.ac.ox.ctl.lti13.OAuth2AuthorizationRequestRedirectFilter;
-import uk.ac.ox.ctl.lti13.security.oauth2.client.lti.authentication.OidcLaunchFlowAuthenticationProvider;
-import uk.ac.ox.ctl.lti13.security.oauth2.client.lti.authentication.TargetLinkUriAuthenticationSuccessHandler;
-import uk.ac.ox.ctl.lti13.security.oauth2.client.lti.web.OAuth2LoginAuthenticationFilter;
-import uk.ac.ox.ctl.lti13.security.oauth2.client.lti.web.OIDCInitiatingLoginRequestResolver;
-
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
-    @Autowired
-    private ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
